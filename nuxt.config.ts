12088988f95518a4cc96fa9ad011c8fae9cd041e
shroomlife 🕉️👨‍💻🍄🌈🍅 <robin@shroomlife.de>
2025-08-21 19:02:05 +0200

    added nuxt ui fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt'],
   ssr: false,
   devtools: { enabled: true },
   // add seo title suffix ssh.studio
